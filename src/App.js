f96eb97cfcbb049eb57506a40b71ddecb8a3e6e9
Teuntitaan1 <Teun.weijdener@gmail.com>
2023-01-24 12:33:55 +0100

    Qr code werkt op computer, nu nog mobiel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import QrReader from 'react-qr-scanner';
 
 // ALL POSSIBLE PROGRAM STATES
 // 1.StartScreen
@@ -7,6 +8,10 @@ import './App.css';
 // 3.AnswerScreen
 // 4.FinishScreen
 // 5. Anything else defaults to the error page
+
+
+//QRCode Formatting should be as followed
+// index of the question should be the containment of the qr code
 
 class App extends React.Component {
   
@@ -23,28 +28,105 @@ class App extends React.Component {
       QuestionsCompleted : 0, 
       // statistics for after the quiz
       Statistics : {"CorrectFirstTime" : 0, "TimeSpent" : 0},
+
+      // qr code variables
+      Scanning : false,
     }
     // event listeners
-    this.ValidateCode = this.ValidateCode.bind(this);
+
+    this.HandleScan = this.HandleScan.bind(this);
+    this.HandleError = this.HandleError.bind(this);
   }
 
-  // all quiz code 
+  // all render code
+  render() {
 
-  ValidateCode(event) {
+    switch (this.state.ProgramState) {
     
-    // prevents the form from reloading the page
-    event.preventDefault();
-    // compares the value from the code input to the correct code
-    if (event.target[0].value === this.state.QuestionList[event.target.id]["Code"]) {
-      
-      console.log("Correct Secret code");
-      // sets the active question and switches screen
-      this.setState({ActiveQuestion: event.target.id});
-      this.SwitchProgramState("AnswerScreen");
-    }
-    else {
-      console.log("Incorrect Secret code");
-    }
+      // loops through all possible program states and determines what to render, no hassle with css styles and its much more efficient
+      case "StartScreen":
+
+        return(
+          <>
+            <h1>{"Welkom bij de Archeon speurtocht over bijen en zijn diverse soorten! (placeholder tekst)"}</h1>
+            <button onClick={() => this.SwitchProgramState("SelectionScreen")}>Start</button>
+          </>
+        );
+
+      case "SelectionScreen":
+
+      // upon completing the question, the checkmark renders, else it does not.
+      // The image has to be required.
+        return (
+          <>
+            <button onClick={() => this.setState({Scanning : !this.state.Scanning})}>Scannen</button>
+            
+            {this.state.Scanning === true ? 
+            
+                <QrReader
+                delay={1000}
+                style={{height: 240, width: 320,}}
+                onError={this.HandleError}
+                onScan={this.HandleScan}
+                />
+                :  null
+            }
+            <ul>
+            {this.state.QuestionList.map((Question, index) => 
+              <li key={index}>
+                {Question["Title"]}
+                {Question["Completed"] ? 
+                <img src={require("./Local_Files/Images/checkmark.png")} alt='Checkmark' className='QuestionCheckmark'/> 
+                : null}
+              </li>
+              )
+                
+            }
+            </ul>
+            <p>{this.state.QuestionsCompleted}/{this.state.AmountOfQuestions} goed beantwoord!</p>
+          </>
+        );  
+
+      case "AnswerScreen":
+    
+        return(
+          <>
+            <h1>{this.state.QuestionList[this.state.ActiveQuestion]["Title"]}</h1>
+            <h2>{this.state.QuestionList[this.state.ActiveQuestion]["Description"]}</h2>
+
+            <button onClick={() => this.ValidateAnswer("Option1")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option1"]}</button>
+            <button onClick={() => this.ValidateAnswer("Option2")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option2"]}</button>
+            <button onClick={() => this.ValidateAnswer("Option3")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option3"]}</button>
+            <button onClick={() => this.ValidateAnswer("Option4")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option4"]}</button>
+
+            <br></br>
+            <button onClick={() => this.SwitchProgramState("SelectionScreen")}>Terug</button>
+          </>
+        );
+
+      case "FinishScreen":
+        
+        return(
+          <>
+            <h1>Hier komt het eindscherm!</h1>
+            <button onClick={() => this.PostStatistics()}>Stuur statistieken op!</button>
+          </>
+          );
+    
+      default:
+
+        return(
+          <>
+            <h1>Error... er is iets fout gegaan!</h1>
+          </>
+        );
+    } 
+  }
+  
+  // all program state code
+  SwitchProgramState(State) {
+    // switches the this.state.ProgramState with the State variable, thus changing the state the program is in and triggering a rerender
+    this.setState({ProgramState : State});
   }
 
   ValidateAnswer(Option) {
@@ -67,28 +149,29 @@ class App extends React.Component {
     }
   }
 
-  // all program state code
-  SwitchProgramState(State) {
-    // switches the this.state.ProgramState with the State variable, thus changing the state the program is in and triggering a rerender
-    this.setState({ProgramState : State});
-  }
+  
 
-  // runs when the program is ready to run
-  componentDidMount() {
+  // all QrCode Code 
+
+  HandleScan(data){
     
-    // reads the file Questions.json and uses it in the quiz
-    var Questions = require("./Local_Files/Quiz_Content/Questions.json");
-    this.setState({QuestionList : Questions});
-    this.setState({AmountOfQuestions : Questions.length});
+    if (data === null) {
+     // do nothing 
+    }
+    else {
+      if (data["text"].length <= 2){
+        this.setState({ActiveQuestion: data["text"]});
+        this.SwitchProgramState("AnswerScreen");
+        this.setState({Scanning : false});
+      }
+    }
 
-    // triggers the code for the statistics
-
+    
   }
-
-  // runs when the program is ready to stop
-  componentWillUnmount() {
+  HandleError(err){
+    
+    console.error(err);
   }
-
   // All statistics code
 
   PostStatistics() {
@@ -104,91 +187,19 @@ class App extends React.Component {
       console.error('Error:', error);
     });
   }
-  // all render code
-  render() {
 
-    switch (this.state.ProgramState) {
-      
-      // loops through all possible program states and determines what to render, no hassle with css styles and its much more efficient
-      case "StartScreen":
+  // runs when the program is ready to run
+  componentDidMount() {
+  
+    // reads the file Questions.json and uses it in the quiz
+    var Questions = require("./Local_Files/Quiz_Content/Questions.json");
+    this.setState({QuestionList : Questions});
+    this.setState({AmountOfQuestions : Questions.length});
 
-        return(
-          <>
-            <h1>{"Welkom bij de Archeon speurtocht over bijen en zijn diverse soorten! (placeholder tekst)"}</h1>
-            <button onClick={() => this.SwitchProgramState("SelectionScreen")}>Start</button>
-          </>
-        );
+  }
 
-      case "SelectionScreen":
-        
-      /////////
-      // upon completing the question, the checkmark renders, else it does not.
-      // the button will render when the question is not yet completed, so the question can be answered.
-      // The image has to be required.
-        return (
-          <>
-            {this.state.QuestionList.map((Question, Index) => 
-              
-              <div key={Index} className='Question'>
-                <h3>{Question["Title"]}</h3>
-
-                {Question["Completed"] ? null :
-                
-                  <form key={Index} id={Index} onSubmit={this.ValidateCode}>
-                  
-                  <input type={'text'}></input>
-                  <input type={'submit'} value={"Controleren"}></input>
-
-                </form>
-                
-                }
-              
-                {Question["Completed"] ? <img src={require('./Local_Files/Images/checkmark.png')} alt='Vraag compleet!' className='QuestionCheckmark'/>: null}
-                
-              </div>
-            )}
-
-            <p>{this.state.QuestionsCompleted}/{this.state.AmountOfQuestions} goed beantwoord!</p>
-            <button onClick={() => this.SwitchProgramState("FinishScreen")}>{"Klaar(Gaat naar FinishScreen( is dus voor testen ))"}</button>
-          </>
-        );  
-
-      case "AnswerScreen":
-        
-        return(
-          <>
-          
-            <h1>{this.state.QuestionList[this.state.ActiveQuestion]["Title"]}</h1>
-            <h2>{this.state.QuestionList[this.state.ActiveQuestion]["Description"]}</h2>
-
-            <button onClick={() => this.ValidateAnswer("Option1")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option1"]}</button>
-            <button onClick={() => this.ValidateAnswer("Option2")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option2"]}</button>
-            <button onClick={() => this.ValidateAnswer("Option3")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option3"]}</button>
-            <button onClick={() => this.ValidateAnswer("Option4")}>{this.state.QuestionList[this.state.ActiveQuestion]["Option4"]}</button>
-
-            <br></br>
-            <button onClick={() => this.SwitchProgramState("SelectionScreen")}>Terug</button>
-
-          </>
-        );
-
-      case "FinishScreen":
-        
-        return(
-          <>
-            <h1>Hier komt het eindscherm!</h1>
-            <button onClick={() => this.PostStatistics()}>Stuur statistieken op!</button>
-          </>
-          );
-    
-      default:
-
-        return(
-          <>
-            <h1>Error... er is iets fout gegaan!</h1>
-          </>
-        );
-    } 
+  // runs when the program is ready to stop
+  componentWillUnmount() {
   }
 }
 
