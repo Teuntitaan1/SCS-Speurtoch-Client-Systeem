3c47eabce4728be4f2007c381a0aa5a79ae4fb6f
Teuntitaan1 <Teun.weijdener@gmail.com>
2023-01-25 14:08:48 +0100

    framework voor styling gelegd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import QrReader from 'react-qr-scanner';
 // Hints
 // Saving State of Quiz
 // Look and feel of the program should be improved
-// Adding support for multiple configurations of question pairs
 
 //QRCode Formatting should be as followed
 // index of the question should be the containment of the qr code
@@ -26,21 +25,21 @@ class App extends React.Component {
 		this.state = {
 		
 			// the state the program currently sits in, look at the top of the file for all allowed states
-		ProgramState : "StartScreen",
-		// all variables used for the quiz itself
-		QuestionList : [],
-		ActiveQuestion : null,
-		
-		AmountOfQuestions : 0,
-		QuestionsCompleted : 0, 
-		// statistics for after the quiz
-		Statistics : {"CorrectFirstTime" : 0, "TimeSpent" : 0},
+			ProgramState : "StartScreen",
+			// all variables used for the quiz itself
+			QuestionList : [],
+			ActiveQuestion : null,
+			
+			AmountOfQuestions : 0,
+			QuestionsCompleted : 0, 
+			// statistics for after the quiz
+			Statistics : {"CorrectFirstTime" : 0, "TimeSpent" : 0},
 
-		// qr code variables
-		Scanning : false,
-		// if the warning is a blank string, nothing will render. else it will
-		// render the warning/error associated with the qr-code
-		Warning : ""
+			// qr code variables
+			Scanning : false,
+			// if the warning is a blank string, nothing will render. else it will
+			// render the warning/error associated with the qr-code
+			Warning : ""
 
 		}
 		// event listeners
@@ -58,10 +57,10 @@ class App extends React.Component {
 		case "StartScreen":
 
 			return(
-			<>
+			<div className='StartScreenDiv'>
 				<h1>{"Welkom bij de Archeon speurtocht over bijen en zijn diverse soorten! (placeholder tekst)"}</h1>
 				<button onClick={() => this.setState({ProgramState : "SelectionScreen"})}>Start</button>
-			</>
+			</div>
 			);
 
 		case "SelectionScreen":
@@ -69,13 +68,13 @@ class App extends React.Component {
 		// upon completing the question, the checkmark renders, else it does not.
 		// The image has to be required.
 			return (
-			<>
+			<div className='SelectionScreenDiv'>
 				<button onClick={() => this.setState({Scanning : !this.state.Scanning})}>Scannen</button>
 				
 				{this.state.Scanning === true ? 
 				
 					<QrReader
-					delay={1000}
+					delay={100}
 					style={{height: 240, width: 320,}}
 					onError={this.HandleError}
 					onScan={this.HandleScan}
@@ -85,24 +84,17 @@ class App extends React.Component {
 				
 				<p style={{"color" : "red"}}>{this.state.Warning}</p>
 
-				{this.state.QuestionList.map((Question, index) => 
-				<p key={index}>
-					{Question["Title"]}
-					{Question["Completed"] ? 
-					<img src={require("./Local_Files/Images/checkmark.png")} alt='Checkmark' className='QuestionCheckmark'/> 
-					: null}
-				</p>
-				)
-					
-				}
-				<p>{this.state.QuestionsCompleted}/{this.state.AmountOfQuestions} goed beantwoord!</p>
-			</>
-			);  
+				{this.state.QuestionList.map((Question, index) =>
+					Question["Completed"] === true ? <CompletedQuestion key={index} Question = {Question}/> : null
+				)}
+			</div> 
+		); 
+
 
 		case "AnswerScreen":
 		
 			return(
-			<>
+			<div className='AnswerScreenDiv'>
 				<h1>{this.state.QuestionList[this.state.ActiveQuestion]["Title"]}</h1>
 				<h2>{this.state.QuestionList[this.state.ActiveQuestion]["Description"]}</h2>
 
@@ -112,54 +104,45 @@ class App extends React.Component {
 					<button key={index} onClick={() => this.ValidateAnswer(Option)}>{Option}</button>)
 				}
 				<button onClick={() => this.setState({ProgramState : "SelectionScreen"})}>Terug</button>
-			</>
+			</div>
 			);
 
 		case "FinishScreen":
 			
 			return(
-			<>
+			<div className='FinishScreenDiv'>
 				<h1>Hier komt het eindscherm!</h1>
 				<button onClick={() => this.PostStatistics()}>Stuur statistieken op!</button>
-			</>
+			</div>
 			);
 		
 		default:
 
 			return(
-			<>
+			<div className='ErrorScreenDiv'>
 				<h1>Error... er is iets fout gegaan!</h1>
-			</>
+			</div>
 			);
 		} 
 	}
   
   	ValidateAnswer(Option) {
 
+		console.log("here")
 		// validates the option given by the user and if correct, marks the question as done, also returns to the selection screen
 		if (Option === this.state.QuestionList[this.state.ActiveQuestion]["CorrectAnswer"]) {
 		
 			var NewQuestionList = this.state.QuestionList;
 			NewQuestionList[this.state.ActiveQuestion]["Completed"] = true;
-			this.setState({QuestionList : NewQuestionList});
-			this.setState({QuestionsCompleted : this.state.QuestionsCompleted + 1});
-
-			// if all questions have been completed, the quiz finishes, else it moves back to the SelectionScreen
-			if(!this.state.QuestionsCompleted === this.state.AmountOfQuestions) {
-				this.setState({ProgramState : "SelectionScreen"});
-			}
-			else {
-				this.setState({ProgramState : "FinishScreen"});	
-			}
-			  
+			this.setState({QuestionList : NewQuestionList});			
+			
+			this.setState({ProgramState : "SelectionScreen"});
 		}
 		else {
 			console.log("Incorrect Answer");
 		}
   }
-
-  
-
+  		
   	// all QrCode Code 
 
   	// handles the scanning of the qr codes
@@ -170,8 +153,8 @@ class App extends React.Component {
 			this.setState({Warning : ""});
 			return null; 
 		}
-		// checks if the qrcode is valid following our guidelines
-		if (data["text"].length >= 2) {
+		// checks if the qrcode is valid by checking if it would return a key in our QuestionList
+		if (this.state.QuestionList[data["text"]] === undefined) {
 			this.setState({Warning : "Deze QR-code is niet geldig, probeer een andere!"});
 			return null; 
 		}
@@ -226,4 +209,22 @@ class App extends React.Component {
 	}
 }
 
+
+
+class CompletedQuestion extends React.Component {
+	render() {
+		return(
+			<div className='CompletedQuestionDiv'>
+				<h3 className='CompletedQuestionTitle'>{this.props.Question["Title"]}</h3>
+				<h4 className='CompletedQuestionDescription'>{this.props.Question["Description"]}</h4>
+				<ul>
+					{this.props.Question["Options"].map((Option, index) => 
+						Option === this.props.Question["CorrectAnswer"] ?
+						<li key={index} className='CorrectAnswer'>{Option}</li> : 
+						<li key={index} className='IncorrectAnswer'>{Option}</li>)}
+				</ul>
+			</div>
+		);
+	}
+} 
 export default App;
