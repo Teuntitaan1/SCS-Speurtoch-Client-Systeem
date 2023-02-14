469c5684eb45ba4c1f1baee0ad5924bc2133e4a2
Teuntitaan1 <Teun.weijdener@gmail.com>
2023-02-14 20:28:51 +0100

    Je boi fixt de leaderboard

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './StyleSheets/index.css';
 import App from './App';
-import { isSafari } from 'react-device-detect';
+import { browserName, browserVersion } from 'react-device-detect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const debug = true;
-const legacymode = isSafari === true ? true : false;
-
+console.log(`BrowserName: ${browserName}, BrowserVersion: ${browserVersion}`);
 
 
 root.render(
   <React.StrictMode>
-    <App debugmode={debug} legacymode={false}/>
+    <App debugmode={debug}/>
   </React.StrictMode>
 );
 
