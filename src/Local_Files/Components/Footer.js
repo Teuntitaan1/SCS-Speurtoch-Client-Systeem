0760c29eab6ab47243801f184df662b196c994ad
Teuntitaan1 <Teun.weijdener@gmail.com>
2023-03-20 23:28:48 +0100

    hoppa volgende layout fix, nu naar bed

--- src/Local_Files/Components/Footer.js
+++ src/Local_Files/Components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends React.Component {
                 <div style={{backgroundColor : "#56a222", width : 100+"%", height : 8+"vh", position : 'absolute', bottom : 0+"%", left : 0+"%"}}>
 					
 					{/*Progress bar*/}
-					<div style={{width : 100+"%", height : 75+"%", position : 'absolute', bottom : 4.5+"vh"}}>
+					<div style={{width : 100+"%", height : 75+"%", position : 'absolute', bottom : 3.5+"vh"}}>
 						<div style={{backgroundColor : 'green', height : 100+"%", width : (this.props.QuestionsCompleted * (100 / this.props.QuestionListLength))+"%", transition: 'width 1s ease-in-out', borderRadius : 5+"px"}}>
 							<p style={{fontWeight : 'bold', textAlign : 'end', position : 'relative', top : 30+"%"}}>{this.props.QuestionsCompleted}/{this.props.QuestionListLength}</p>
 						</div>
